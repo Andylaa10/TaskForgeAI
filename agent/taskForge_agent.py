b293f9174a236035bf4aafd01dd0fdf601706ee8
KristianHollaender <kris13r0@easv365.dk>
2024-12-05 18:59:30 +0100

    new prompt

--- agent/taskForge_agent.py
+++ agent/taskForge_agent.py
@@ -3,16 +3,14 @@ from config.config import LLM_CONFIG
 
 # Define the system prompt for TaskForgeAgent
 system_prompt = """
-You are TaskForge, an AI agent specialized in breaking down high-level tasks into actionable subtasks.
+You are TaskForge, an AI agent specialized in breaking down high-level tasks into subtasks.
 
 You have the following tool available:
 - `read_task_from_file`: Reads content from a specified file path.
 - `get_owner_id`: Get owner of the github account and returns the id. The owner id needs to saved and used as an argument by create_project tool
 - `create_project`: Create Github project by taking the id of the owner and the generated project_name as arguments
 
-
-Your job is to take a high level task ( which is read via the read_content_of_file method ), create a project name of 3 words and then divide the tasks into smaller subtasks.
-First of you should make a project name, and then -> To be specific, these subtasks should be created from the task in task.txt and each subtask should consist of the following:
+These subtasks should be created from the task in task.txt and have a title and each subtask should consist of the following:
 - title
 - description
 - time_estimate
@@ -36,22 +34,9 @@ Example output:
     ]
 }
 
-VERY IMPORTANT THAT YOU FOLLOW THIS
-RULE: 
-1. DO NOT USE SELF MADE TOOLS!!!
-2. ONLY CALL EACH TOOL ONCE!!!
-
-**YOU MUST FOLLOW THESE STEPS!!!!!!!!!!!
-1. First use the read_content_of_file tool to read the content of the file and do whatever it says.
-2. Return the example output with the 'project name' and subtasks.
-3. CALL the get_owner_id tool and save it because we need it in another tool calling. Return the owner id.
-4. PLEASE CALL the create_project tool with the id of the owner and the 'project name' as arguments. Return the project id!!!!!
-6. Return the example output.
-7. TERMINATE (uppercase, as the final word in your response).**
-
-When all steps above are done:
-- Write TERMINATE (it should always be UPPERCASE and the last word in the response at all time).
+Return 'TERMINATE' when the task is done.
 """
+
 def create_task_forge_agent() -> AssistantAgent:
     agent = AssistantAgent(
         name="Task Forge Agent",
